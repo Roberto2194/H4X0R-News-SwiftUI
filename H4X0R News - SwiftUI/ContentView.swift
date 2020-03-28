570ccb2d10b1bd0b9a69c2a1d8657bfec8672128
Roberto Liccardo <roberto_liccardo@outlook.it>
2020-03-28 10:49:41 +0100

    Initial Commit

---
+++ H4X0R News - SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News - SwiftUI
+//
+//  Created by Roberto Liccardo on 28/03/2020.
+//  Copyright © 2020 Roberto Liccardo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
